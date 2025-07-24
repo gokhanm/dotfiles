2298b356ffb19c85893c2b8bcc2a87e90af34eb3
Gokhan MANKARA <gokhanm@gmail.com>
2025-07-24 11:52:59 +0300

    remove \n line

--- fish/functions/gplo.fish
+++ fish/functions/gplo.fish
@@ -1,4 +1,3 @@
-function gplo --wraps='git pull origin (git rev-parse --abbrev-ref HEAD)'
-    --description 'alias gplo=git pull origin (git rev-parse --abbrev-ref HEAD)'
+function gplo --wraps='git pull origin (git rev-parse --abbrev-ref HEAD)' --description 'alias gplo=git pull origin (git rev-parse --abbrev-ref HEAD)'
   git pull origin (git rev-parse --abbrev-ref HEAD) $argv;
 end
