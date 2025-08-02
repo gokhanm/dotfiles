9ef247182438c4162675b29ccbc484ef4aee7ca7
Gokhan MANKARA <gokhanm@gmail.com>
2025-08-02 09:43:21 +0300

    set brew to fish path

--- config.fish
+++ config.fish
@@ -46,3 +46,10 @@ set -g fish_user_paths $GOBIN $fish_user_paths
 set -g fish_user_paths "/opt/homebrew/bin" $fish_user_paths
 set -g fish_user_paths $RUSTBIN $fish_user_paths
 
+if test -d (brew --prefix)"/share/fish/completions"
+    set -p fish_complete_path (brew --prefix)/share/fish/completions
+end
+
+if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+    set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+end
